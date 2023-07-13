77ddc95b547da4aca4aa816115eba27491907756
J <pravj@informatica.com>
2023-07-13 11:58:21 +0530

    my first commit statment

---
+++ src/main/java/org/example/DemoEx1.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+public class DemoEx1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello pooja nm ........");
+        System.out.println("how are you pooja nm...");
+    }
+}
